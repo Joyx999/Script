8a0869244a7846d551aea87bc25831131ca4ca1d
Joyx999 <96812713+Joyx999@users.noreply.github.com>
2021-12-29 13:52:18 +0700

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 local key = _G.Key
 local check = "https://raw.githubusercontent.com/Joyx999/Script/main/check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("scripthere"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joyx999/Script/main/dahoodscript"))()
 else
 game.Players.LocalPlayer:Kick("
 Check your key stupid !!!")
